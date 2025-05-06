bbe04b163bcffdf76615af4322e962894b03f580
Sabrina Ira <85188006+sabrinaira@users.noreply.github.com>
2025-05-06 00:15:52 -0400

    added functions practice & .gitignore file

---
+++ functions.js
@@ -0,0 +1,15 @@
+const sum = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(sum(10, 5));
+
+// const multiply = (num1, num2) => {
+//   return num1 * num2;
+// };
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multiply(10, 5));
---
+++ functions.py
@@ -0,0 +1,13 @@
+def sum(num1, num2):
+    return num1 + num2
+
+
+print(sum(10, 5))
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+print(multiply(10, 5))
+
